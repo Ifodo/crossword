2952c1fd42a299152c35c5b9ee052f7f0c2ede87
Ifodo <ifodomartin@gmail.com>
2025-10-09 08:56:50 +0100

    Update contact name from Madam Boro to Miss Smart

--- src/app.js
+++ src/app.js
@@ -165,7 +165,7 @@
           <p>Chat with any of our representatives:</p>
           <div class="contact-details">
             <div class="contact-person">
-              <strong>Madam Boro</strong><br>
+              <strong>Miss Smart</strong><br>
               <a href="https://wa.me/2349165226722" target="_blank" class="whatsapp-link">
                 📱 +234 916 522 6722
               </a>
